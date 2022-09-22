3c5e4d868d58434a505f3f7693015a05320f7508
henzpp <lynn5863@hotmail.com>
2022-09-23 08:53:48 +0900

    0922

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.blog),
+    #path('<int:pk>/', views.PostDetail.as_view()),
+    #path('', views.PostList.as_view()),
+    path('community/', views.community),
+    path('forums/', views.forums),
+    path('gallery/', views.gallery),
+    path('plantrvl/', views.plantrvl),
+    
 ]
-
--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,62 @@
+from django.views.generic import ListView, DetailView
+from .models import Post
 from django.shortcuts import render
 
-# Create your views here.
+class PostList(ListView):
+    model = Post
+    ordering = '-pk'
 
-def blog(request):
+class PostDetail(DetailView):
+    model = Post
+
+
+
+def community(request):
     return render(
         request,
-        'blog/blog.html',
+        'blog/community.html'
     )
 
+def forums(request):
+    return render(
+        request,
+        'blog/forums.html'
+    )
+
+def gallery(request):
+    return render(
+        request,
+        'blog/gallery.html'
+    )
+
+def plantrvl(request):
+    return render(
+        request,
+        'blog/plantrvl.html'
+    )
+
+
+class Forums(ListView):
+    model = Post
+    ordering = '-pk'
+    paginate_by=5
+
+
+
+# def blog(request):
+#     posts = Post.objects.all()
+#     return render(
+#         request,
+#         'blog/blog.html',
+#     )
+
+# def community(request, pk):
+#     post = Post.objects.get(pk=pk)
+
+#     return render(
+#         request,
+#         'blog/community.html',
+#         {
+#             'post' : post,
+#         }
+#     )
